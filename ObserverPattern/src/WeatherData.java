c42f6b00e88ac5dd0eb12bf4695a03a1dfdd1b81
Rachana Acharya <rasnacharya@gmail.com>
2018-01-01 14:51:07 -0700

    Initial commit

---
+++ ObserverPattern/src/WeatherData.java
@@ -0,0 +1,16 @@
+public class WeatherData {
+
+    // instance varaibale declarations
+
+    public void measurementsChanged() {
+        float temp = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+
+        currentConditionsDisplay.update(temp, humidity, pressure);
+        statisticsDisplay.update(temp, humidity, pressure);
+        forecastDisplay.update(temp, humidity, pressure);
+    }
+
+    // other WeatherData methods here
+}
