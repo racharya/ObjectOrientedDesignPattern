fb8302549b2573429d9cab4d9ccdbd3d707983f1
Rachana Acharya <rasnacharya@gmail.com>
2018-01-14 21:17:19 -0700

    Checkout the menu items

--- src/IteratorAndCompositePatterns/MenuItem.java
+++ src/IteratorAndCompositePatterns/MenuItem.java
@@ -1,4 +1,31 @@
 package IteratorAndCompositePatterns;
 
 public class MenuItem {
+    String name;
+    String description;
+    boolean vegeterian;
+    double price;
+
+    public MenuItem(String name, String description, boolean vegeterian, double price){
+        this.name = name;
+        this.description = description;
+        this.vegeterian = vegeterian;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isVegeterian() {
+        return vegeterian;
+    }
 }
