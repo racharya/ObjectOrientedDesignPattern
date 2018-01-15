d75d078b17ff87b3523f5f2c69c4d7a8c308a215
Rachana Acharya <rasnacharya@gmail.com>
2018-01-14 21:26:59 -0700

    Pancake house menu items implementation

---
+++ src/IteratorAndCompositePatterns/PancakeHouseMenu.java
@@ -0,0 +1,28 @@
+package IteratorAndCompositePatterns;
+
+import java.util.ArrayList;
+
+public class PancakeHouseMenu {
+    ArrayList menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList();
+
+        addItem("K&B's Pancake Breakfast", "Pancakes with scrambled eggs, and toast", true, 2.99);
+        addItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
+        addItem("Blueberry Pancake", "Pancakes made with fresh blueberries", true, 3.49);
+        addItem("Waffles", "Waffles, with your choice of blueberries or strawberries", true, 3.59);
+
+    }
+
+    public void addItem(String name, String description, boolean vegeterian, double price){
+        MenuItem menuItem = new MenuItem(name, description, vegeterian,price);
+        menuItems.add(menuItem);
+    }
+
+    public ArrayList getMenuItems() {
+        return menuItems;
+    }
+
+    // other menu methods
+}
