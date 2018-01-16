bff5a52c2d0cd9d14046579907fa5715e278a595
Rachana Acharya <rasnacharya@gmail.com>
2018-01-15 23:01:47 -0700

    Implement concrete iterator that works for the Diner menu

--- src/IteratorAndCompositePatterns/DinerMenuIterator.java
+++ src/IteratorAndCompositePatterns/DinerMenuIterator.java
@@ -1,4 +1,21 @@
 package IteratorAndCompositePatterns;
 
-public class DinerMenuIterator {
+public class DinerMenuIterator implements Iterator {
+
+    MenuItem[] items;
+    int position = 0;
+
+    public DinerMenuIterator(MenuItem[] items){
+        this.items = items;
+    }
+
+    public Object next() {
+        MenuItem menuItem = items[position];
+        position += 1;
+        return menuItem;
+    }
+
+    public boolean hasNext(){
+        return (position >= items.length || items[position] == null) ? false : true;
+    }
 }
