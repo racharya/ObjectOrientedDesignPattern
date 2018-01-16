4da17d70554066407f08701b0bb36a48fd1c8b50
Rachana Acharya <rasnacharya@gmail.com>
2018-01-15 23:01:04 -0700

    Implement Iterator interface

---
+++ src/IteratorAndCompositePatterns/Iterator.java
@@ -0,0 +1,8 @@
+package IteratorAndCompositePatterns;
+
+public interface Iterator {
+
+    boolean hasNext();
+
+    Object next();
+}
---
+++ src/IteratorAndCompositePatterns/PancakeHouseIterator.java
@@ -0,0 +1,4 @@
+package IteratorAndCompositePatterns;
+
+public class PancakeHouseIterator {
+}
