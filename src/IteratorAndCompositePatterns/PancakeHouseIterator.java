f47c58101d245eb67441a003ede4e2d964d0d9f2
Rachana Acharya <rasnacharya@gmail.com>
2018-01-15 23:02:08 -0700

    Implement concrete iterator that works for the PancakeHouse menu

--- src/IteratorAndCompositePatterns/PancakeHouseIterator.java
+++ src/IteratorAndCompositePatterns/PancakeHouseIterator.java
@@ -1,4 +1,23 @@
 package IteratorAndCompositePatterns;
 
-public class PancakeHouseIterator {
+import java.util.ArrayList;
+
+public class PancakeHouseIterator implements Iterator {
+
+    ArrayList<MenuItem> items;
+    int position = 0;
+
+    public PancakeHouseIterator(ArrayList items){
+        this.items = items;
+    }
+
+    public Object next(){
+        MenuItem item = items.get(position);
+        position += 1;
+        return item;
+    }
+
+    public boolean hasNext() {
+        return (items.get(position) == null || position >= items.size()) ? false : true ;
+    }
 }
